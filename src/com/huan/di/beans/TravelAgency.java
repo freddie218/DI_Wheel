ffce133e0514e229a7df6b9e79eeb90f1cf4e830
Huan.Wang <freddie218@gmail.com>
2013-04-09 23:19:07 +0800

    Add a travel_agency class for beans consuming. huan

---
+++ src/com/huan/di/beans/TravelAgency.java
@@ -0,0 +1,28 @@
+package com.huan.di.beans;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: twer
+ * Date: 4/9/13
+ * Time: 11:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TravelAgency {
+
+    public static void main(String[] args) {
+
+        Travel travel = new Travel(new Train());
+        Location xian = new Location("xian");
+        Location lanzhou = new Location("lanzhou");
+        Location zhangye = new Location("zhangye");
+        Location dunhuang = new Location("dunhuang");
+
+        travel.addRoute(new Route(xian, lanzhou, 647));
+        travel.addRoute(new Route(lanzhou, zhangye, 511));
+        travel.addRoute(new Route(zhangye, dunhuang, 594));
+        travel.addRoute(new Route(dunhuang, xian, 1730));
+
+        double cost = travel.transportationCost();
+        System.out.print("totla travel cost is: " + cost);
+    }
+}
